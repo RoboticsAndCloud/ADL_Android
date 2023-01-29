da55dd08f5d789d30adb71cfc484b39595d1842c
ASCC Lab <lonefeifei@126.com>
2023-01-28 23:19:41 -0600

    send data to robot

--- CameraFragment/app/src/main/java/com/github/florent37/camerafragment/sample/TestMain.java
+++ CameraFragment/app/src/main/java/com/github/florent37/camerafragment/sample/TestMain.java
@@ -3,7 +3,7 @@ package com.github.florent37.camerafragment.sample;
 public class TestMain {
     public static void main(String []args){
 //        System.out.println("Hello");
-        CommunicationService communicationService = new CommunicationService();
+//        CommunicationService communicationService = new CommunicationService();
         String ipsend = "10.227.102.0";
         int port = 59000;
         int cnt = 1;
@@ -13,8 +13,11 @@ public class TestMain {
         String motion_filename =  "/home/ascc/Desktop/motion.txt";
 //        communicationService.socketImageSendingHandler(ipsend, port, cnt, currentTime, image_filename);
 
-        communicationService.socketAudioSendingHandler(ipsend, port, currentTime, audio_filename);
+//        communicationService.socketAudioSendingHandler(ipsend, port, currentTime, audio_filename);
 //        communicationService.socketMotionSendingHandler(ipsend, port, currentTime, motion_filename);
+
+        new CommunicationImageService().execute(ipsend, String.valueOf(port), "1", currentTime, image_filename);
+
 
     }
 }
